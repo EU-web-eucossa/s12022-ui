98cb63d1157cb19112f5859a7e54f4bb3140db69
Felix Orinda <forinda82@gmail.com>
2022-09-28 09:34:58 +0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        'c-text':['Crimson Text','sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
