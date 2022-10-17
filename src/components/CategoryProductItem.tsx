550da49376a5d1e241ef8815261053d5893f8ab7
Felix Orinda <forinda82@gmail.com>
2022-10-17 15:57:07 +0300

    Add categories on homepage

---
+++ src/components/CategoryProductItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CategoryProductItem = () => {
+	return (
+		<div>
+			<div className="">
+				<img src="" alt="" />
+				<div></div>
+			</div>
+			<div></div>
+		</div>
+	);
+};
+
+export default CategoryProductItem;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Homecategories from '../components/Homecategories';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import slugify from '../helpers/slugify';
@@ -9,29 +10,36 @@ const Homepage = () => {
 	const { products } = useAppSelector((state) => state.products);
 
 	return (
-		<div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 p-4">
-			{products.map((p) => (
-				<div key={p.name} className="flex flex-col gap-4">
-					<Link to={`/product/${slugify(p.name)}`} className="w-100">
-						<img src={p.featuredImage} alt="" className="w-full object-cover" />
-					</Link>
-					<div>
-						<h2 className="text-xl font-bold capitalize">{p.name}</h2>
+		<div>
+			<Homecategories/>
+			<div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 p-4">
+				{products.map((p) => (
+					<div key={p.name} className="flex flex-col gap-4">
+						<Link to={`/product/${slugify(p.name)}`} className="w-100">
+							<img
+								src={p.featuredImage}
+								alt=""
+								className="w-full object-cover"
+							/>
+						</Link>
 						<div>
-							{starGenerator(p.ratings).map((s, i) => (
-								<FontAwesomeIcon
-									key={i}
-									icon={s}
-									color="#F58634"
-									className="cursor-pointer"
-								/>
-							))}
+							<h2 className="text-xl font-bold capitalize">{p.name}</h2>
+							<div>
+								{starGenerator(p.ratings).map((s, i) => (
+									<FontAwesomeIcon
+										key={i}
+										icon={s}
+										color="#F58634"
+										className="cursor-pointer"
+									/>
+								))}
+							</div>
+							<div className="text-lg font-bold">${p.price}</div>
+							<div>Price {p.price}</div>
 						</div>
-						<div className="text-lg font-bold">${p.price}</div>
-						<div>Price {p.price}</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	);
 };
