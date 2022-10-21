f8d1d8f5194522752406bed121112c83edc0d028
Felix Orinda <forinda82@gmail.com>
2022-10-21 14:28:20 +0300

    Fix layout and update login and signup pages

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,66 +1,76 @@
 import AboutPage from '../pages/AboutPage';
+import BaseLayout from '../layouts/BaseLayout';
 import CartPage from '../pages/CartPage';
 import CategoriesPage from '../pages/CategoriesPage';
 import ContactPage from '../pages/ContactPage';
+import EmptyLayout from '../layouts/EmptyLayout';
 import Homepage from '../pages/Homepage';
 import { IRouteProps } from '../interfaces/router';
 import LoginPage from '../pages/LoginPage';
 import NotFound from '../pages/NotFound';
+import ProfilePage from '../pages/ProfilePage';
 import React from 'react';
 import SignupPage from '../pages/SignupPage';
 import SingleProductPage from '../pages/SingleProductPage';
+import layoutWrap from '../helpers/LayoutWrapper';
 
 const routes: IRouteProps[] = [
 	{
-		Component: <Homepage />,
+		Component: layoutWrap(BaseLayout, Homepage),
 		isNested: false,
 		pathName: 'Home',
 		urlPath: '/'
 	},
 	{
-		Component: <SingleProductPage />,
+		Component: layoutWrap(BaseLayout, SingleProductPage),
 		isNested: false,
 		pathName: 'SingleProductPage',
 		urlPath: '/product/:id'
 	},
 	{
-		Component: <CategoriesPage />,
+		Component: layoutWrap(BaseLayout, CategoriesPage),
 		isNested: false,
 		pathName: 'categories',
 		urlPath: '/categories'
 	},
 	{
-		Component: <AboutPage />,
+		Component: layoutWrap(BaseLayout, AboutPage),
 		isNested: false,
 		pathName: 'About',
 		urlPath: '/about'
 	},
 	{
-		Component: <ContactPage />,
+		Component: layoutWrap(BaseLayout, ContactPage),
 		isNested: false,
 		pathName: 'Contact',
 		urlPath: '/contact'
 	},
 	{
-		Component: <LoginPage />,
+		Component: layoutWrap(EmptyLayout, LoginPage),
 		pathName: 'Login',
-		isNested:false,
+		isNested: false,
 		urlPath: '/login'
 	},
 	{
-		Component: <SignupPage />,
-		pathName: 'Signup',
-		urlPath: '/signup',
-		isNested:false
+		Component: layoutWrap(EmptyLayout, ProfilePage),
+		pathName: 'Login',
+		isNested: false,
+		urlPath: '/login'
 	},
 	{
-		Component: <CartPage />,
+		Component: layoutWrap(EmptyLayout, SignupPage),
+		pathName: 'Signup',
+		urlPath: '/signup',
+		isNested: false
+	},
+	{
+		Component: layoutWrap(BaseLayout, CartPage),
 		pathName: 'Cart',
 		urlPath: '/cart',
 		isNested: false
 	},
 	{
-		Component: <NotFound />,
+		Component: layoutWrap(BaseLayout, NotFound),
 		isNested: false,
 		pathName: 'NotFound',
 		urlPath: '*'
