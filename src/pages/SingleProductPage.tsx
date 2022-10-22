aa91c9bc553b84d299dedae5c0958d1588ca856f
Rose Odhiambo <odhiamborose466@gmail.com>
2022-10-22 20:56:30 +0300

    updated cart quantity

--- src/pages/SingleProductPage.tsx
+++ src/pages/SingleProductPage.tsx
@@ -23,10 +23,26 @@ const SingleProductPage = () => {
 		<div>
 			{currentProduct && (
 				<div className=" flex py-5">
-					<img src={currentProduct.featuredImage} alt="" className="h-80" />
-					<div className='pl-8'>
-						<h1 className="text-4xl font-bold capitalize">{currentProduct.name}</h1>
-						<div className='pt-3'>
+					<div className="flex flex-col gap-3">
+						<img src={currentProduct.thumbnail} alt="" className="h-80" />
+						<p>{currentProduct.description}</p>
+						<div className="flex gap-2">
+							{currentProduct.images.length > 0 &&
+								currentProduct.images.map((image, i) => (
+									<img
+										key={i}
+										src={image}
+										alt=""
+										className="h-32 w-32 object-cover border rounded-md border-black shadow-lg hover:scale-110 cursor-pointer transition-all ease-linear duration-300"
+									/>
+								))}
+						</div>
+					</div>
+					<div className="pl-8">
+						<h1 className="text-4xl font-bold capitalize">
+							{currentProduct.title}
+						</h1>
+						<div className="pt-3">
 							{starGenerator(currentProduct.rating).map((s, i) => (
 								<FontAwesomeIcon
 									key={i}
@@ -49,17 +65,19 @@ const SingleProductPage = () => {
 							ADD TO CART
 						</button>
 					</div>
+					<div className='flex flex-col justify-center items-center'>
+						<h4>More Like This</h4>
+						<div className='flex  justify-around items-center'>
+							<button className='rounded-full p-2 mx-24 border border-black'>Polly Necks</button>
+							<button className='rounded-full p-2 mx-24 border border-black'>Resian Skirts</button>
+							<button className='rounded-full p-2 mx-24 border border-black'>Resian Dresses</button>
+						</div>
+					</div> 
 				</div>
 			)}
-			<div className='flex flex-col justify-center items-center'>
-				<h4>More Like This</h4>
-				<div className='flex  justify-around items-center'>
-					<button className='rounded-full p-2 mx-24 border border-black'>Polly Necks</button>
-					<button className='rounded-full p-2 mx-24 border border-black'>Resian Skirts</button>
-					<button className='rounded-full p-2 mx-24 border border-black'>Resian Dresses</button>
-				</div>
-			</div>
 		</div>
+			
+	
 	);
 };
 
