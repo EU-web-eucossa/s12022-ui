26e688888efdd25ac56329744fa96c9fba76101b
Felix Orinda <forinda82@gmail.com>
2022-11-11 17:51:32 +0300

    Update checkout

--- src/components/PayPal.tsx
+++ src/components/PayPal.tsx
@@ -2,65 +2,23 @@
 /* eslint-disable camelcase */
 import React from 'react';
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { config } from '../api/config';
-import { useAppSelector } from '../state/hooks';
-import { PurchasePaypalItemType } from '../types';
 
 export default function Paypal() {
-	const { cartProducts, totalPrice, totalQuantity } = useAppSelector(state => state.root.cart)
-	const paypalOptions = {
-		'client-id': config.paypalClientId,
+	const initialOptions = {
+		'client-id':
+			'ARlD4eYBquW5PAihq6LsSchvHfLyo-7t8-P3wjxyQqVV4NjgcpfQgBkABHqssSs4m3HaA7o72_45b30v',
+		currency: 'USD',
 		intent: 'capture',
-		currency: 'USD'
+		'data-client-token': 'abc123xyz=='
 	};
-	const items: PurchasePaypalItemType[] = cartProducts.map((item) => (
-		{
-			name: item.name,
-			unit_amount: {
-				currency_code: 'USD',
-				value: (item.price * item.quantity) as unknown as string,
-			},
-			quantity: item.quantity as unknown as string,
-		}
-	));
-	const payee = {
-		name: {
-			given_name: 'Mike',
-			surname: 'Juma'
-		},
-		email_address: "",
-		phone: {
-			phone_type: "MOBILE",
-			phone_number: {
-				national_number: "1234567890"
-			}
-		},
-		address: {
-			address_line_1: "123 Townsend St",
-			address_line_2: "Floor 6",
-			admin_area_2: "San Francisco",
-			admin_area_1: "CA",
-			country_code: "US",
-			postal_code: "94107"
-		},
-		birth_date: "1990-01-01",
-		payer_id: "1234567890",
-		tax_info: {
-			tax_id: "123456789",
-			tax_id_type: "BR_CPF"
-		}, tenant: '1234567890'
-	}
 
 	return (
-		<div className='flex items-center flex-col justify-center'>
-			<div>
-				Pay for ${totalPrice}
-			</div>
-
-			<PayPalScriptProvider options={{
+		<PayPalScriptProvider
+			options={{
 				'client-id':
 					'ARlD4eYBquW5PAihq6LsSchvHfLyo-7t8-P3wjxyQqVV4NjgcpfQgBkABHqssSs4m3HaA7o72_45b30v'
-			}}>
+			}}
+		>
 			<PayPalButtons
 				style={{ layout: 'vertical' }}
 				createOrder={(data, actions) => {
@@ -70,12 +28,8 @@ export default function Paypal() {
 						purchase_units: [
 							{
 								amount: {
-									value: '0.01',
-									currency_code: 'USD'
-
-								},
-								payee,
-								items,
+									value: '0.01'
+								}
 							}
 						]
 					});
@@ -84,15 +38,10 @@ export default function Paypal() {
 					console.log('Appoved', data);
 
 					return actions.order!.capture().then(function (details) {
-						console.log(details);
-
 						alert('Transaction completed by ' + details.payer.name!.given_name);
 					});
 				}}
 			/>
-			</PayPalScriptProvider>
-		</div>
+		</PayPalScriptProvider>
 	);
 }
-
-
