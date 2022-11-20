e85891a676441389a75638f4b655396c09738e2e
Felix Orinda <forinda82@gmail.com>
2022-11-20 12:35:58 +0300

    Remove extra logs

--- src/pages/Admin/DashSummary.tsx
+++ src/pages/Admin/DashSummary.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { CatCounter } from '../../hooks/CatCounter';
 import DashBoardCard from './components/DashBoardCard';
 import { ProductcategoryType } from '../../types';
